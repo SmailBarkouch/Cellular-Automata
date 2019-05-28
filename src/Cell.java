71324b728c2467ff22b76406ee8b3b6e1bfb27de
Smail <728378@gmail.com>
2019-05-28 16:49:10 +0000

    added comments to code, not sure if I should add reset state method

--- src/Cell.java
+++ src/Cell.java
@@ -1,54 +1,54 @@
 public class Cell
 {
-    private int state;
-    private int prevState;
-    private int neighbors;
+    private int state; // Will be either 0 or 1, 0 denoting a dead state and 1 denoting an alive state
+    private int prevState; // The state of a cell from the last generation
+    private int neighbors; // The amount of live neighbors around a cell, at most 8
     
-    public Cell ()
+    public Cell () // setting all values to 0 since they need to be updated
     {
-        state = 0;
+        state = 0; // all cells are orignally dead util set otherwise or by the player
+        prevState = 0;
+        neighbors = 0; // unchecked
+    }
+    
+    public Cell (int state) // setting all values to 0, but state becomes 1 as per player choice
+    {
+        this.state = state; // becomes 1
         prevState = 0;
         neighbors = 0;
     }
     
-    public Cell (int state)
-    {
-        this.state = state;
-        prevState = 0;
-        neighbors = 0;
-    }
-    
-    public void incrNeighbors ()
+    public void incrNeighbors () // rather than setting a neighbor value, incrementing is easier
     {
         neighbors++;
     }
     
-    public void setState (int state)
+    public void setState (int state) // changes state to 0 or 1 based on rules, and prevState updated
     {
-        prevState = this.state;
-        this.state = state;
+        prevState = this.state; // becomes current state
+        this.state = state; // state updated
     }
     
-    public int getState ()
+    public int getState () // returns the currect state
     {
         return state;
     }
     
-    public int getPrevState ()
+    public int getPrevState () // returns previous state
     {
         return prevState;
     }
     
-    public void updState ()
+    public void updState () // changes the state based on neighbors
     {
-        if(neighbors < 2 || neighbors > 3)
+        if(neighbors < 2 || neighbors > 3) // these are death cases, from overpopulation or underpopulation
         {
             prevState = state;
             state = 0;
-        } else if (neighbors == 3) {
+        } else if (neighbors == 3) { // reproduction case
             prevState = state;
             state = 1;
-        } else {
+        } else { // an alive cell stayes alive
             prevState = state;
         }
     }
