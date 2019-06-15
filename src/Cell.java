aeac149af8b97eff2408bebcec6bb263b321d9dd
Smail <728378@gmail.com>
2019-06-15 00:47:51 +0000

    Added comments and getters/setters.

--- src/Cell.java
+++ src/Cell.java
@@ -2,10 +2,16 @@ package us.sbarkouch.gameoflife;
 
 public class Cell
 {
+	/*
+	 * Holds the state of the object, the state it used to have and the neighbors present around it.
+	 */
     private int state; 
     private int prevState; 
     private int neighbors; 
     
+    /*
+	 * Constructs a default dead cell.
+	 */
     public Cell () 
     {
         state = 0; 
@@ -13,6 +19,10 @@ public class Cell
         neighbors = 0; 
     }
     
+    
+    /*
+	 * Constructs a cell with a custom state, only used to give an alive cell.
+	 */
     public Cell (int state) 
     {
         this.state = state; 
@@ -20,45 +30,67 @@ public class Cell
         neighbors = 0;
     }
     
-    public void incrNeighbors () // rather than setting a neighbor value, incrementing is easier
+    /*
+	 * Used to increment the number of neighbors.
+	 */ 
+    public void incrNeighbors () 
     {
         neighbors++;
     }
     
-    public void setState (int state) // changes state to 0 or 1 based on rules, and prevState updated
+    /*
+	 * Changes state to 0 or 1 based on rules, and prevState updated.
+	 */  
+    public void setState (int state) 
     {
         prevState = this.state; // becomes current state
         this.state = state; // state updated
     }
     
+    /*
+	 * Returns the current state.
+	 */  
     public int getState () // returns the currect state
     {
         return state;
     }
     
+    /*
+	 * Returns the current previous state.
+	 */  
     public int getPrevState () // returns previous state
     {
         return prevState;
     }
     
+    /*
+	 * Resets all neighbors.
+	 */  
     public void resetNeigh()
     {
     	neighbors = 0;
     }
     
+    /*
+	 * Returns the amount of neighbors around the cell.
+	 */  
     public int getNeigh()
     {
     	return neighbors;
     }
     
-    public void updState () // changes the state based on neighbors
+    /*
+	 * Changes its state and previous state based on the instance variable neighbors,
+	 * holding the amount of neighbors in the cell.
+	 */  
+    public void updState () 
     {
     	prevState = state;
-        if(neighbors < 2 || neighbors > 3) // these are death cases, from overpopulation or underpopulation
+        if(neighbors < 2 || neighbors > 3) 
         {
             state = 0;
         } 
-        if (neighbors == 3) // reproduction case
+        if (neighbors == 3) 
         {
         	state = 1;
         }
