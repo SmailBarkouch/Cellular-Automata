75501b4edc0388d2c7d4dfc29e8e8b66ed6fb681
Smail <728378@gmail.com>
2019-06-28 15:59:18 +0000

    Board is able to be editied in many ways, ie size and other sorts, finished a while ago just added

--- src/Board.java
+++ src/Board.java
@@ -4,15 +4,13 @@ import java.awt.Color;
 import java.awt.Dimension; 
 import java.awt.Graphics; 
 import java.awt.Point;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame; 
 
-public class Board extends JComponent implements MouseListener, ComponentListener
+public class Board extends JComponent implements MouseListener
 {
 	/*
 	 * Used variables to control board graphics as well as determine the number of
@@ -20,50 +18,45 @@ public class Board extends JComponent implements MouseListener, ComponentListene
 	 */
 	private static final long serialVersionUID = 1L;
 	private Cell[][] cel; 
-	private Cell[][] tempCel;
-	public Dimension celDem;
-	public static final int split = 50;
+	private Dimension celDem;
+	private int split = 50;
 	private int colorSch;
-	private Point loc;
-    
-	/*
-	 * Constructor that creates all cells in this given run
-	 * and creates the tempCel to store uneditied boards for
-	 * logic.
-	 */
-    public Board () 
-    {
-    	cel = new Cell[split][split];
-    	tempCel = new Cell[split][split];
-        for(int height = 0; height < split; height++)
-        {
-        	for(int width = 0; width < split; width++)
-        	{
-        		cel[height][width] = new Cell();
-        		tempCel[height][width] = new Cell();
-        	}
-        }
-        
-        celDem = new Dimension(10,10); 
-        }
+	private Point loc1;
+	private Point loc2;
+	private long delay;
+	private int genPerDelay;
+	private int res;
+	private int dead1;
+	private int dead2;
+	private boolean gridOp;
     
 	/*
 	 * Uses the JFrame to construct the Board. tempCel
 	 * is also constructed and used for logic.
 	 */
-    public Board (JFrame j) 
+    public Board (JFrame j, int res, int dead1, int dead2) 
     {
     	celDem = new Dimension();
-    	tempCel = new Cell[split][split];
+    	delay = 150;
+    	genPerDelay = 1;
+    	this.res = res;
+    	this.dead1 = dead1;
+    	this.dead2 = dead2;
+    	gridOp = true;
+    	resetBoard();
+    }
+    
+    public void resetBoard()
+    {
     	cel = new Cell [split][split];
     	for(int height = 0; height < split; height++)
         {
         	for(int width = 0; width < split; width++)
         	{
-        		cel[height][width] = new Cell();
-        		tempCel[height][width] = new Cell();
+        		cel[height][width] = new Cell(0, res, dead1, dead2);
         	}
         }
+    	repaint();
     }
     
 	/*
@@ -72,6 +65,7 @@ public class Board extends JComponent implements MouseListener, ComponentListene
 	 */
     public void paintComponent (Graphics g) 
     {
+    	celDem = getRootPane().getSize();
     	Color temp;
         for(int width = 0; width < cel.length; width++) 
         {
@@ -84,12 +78,27 @@ public class Board extends JComponent implements MouseListener, ComponentListene
                 } else {
                 	temp = detCelColBW(cel[height][width].getState());
                 }
-                	g.setColor(temp); 
+                if(temp == Color.WHITE && gridOp)
+                {
+                	g.setColor(temp);
                     g.fillRect((int) p.getX(), (int) p.getY(), (int) celDem.getWidth() / split, (int) celDem.getHeight() / split);
-                    g.setColor(Color.BLACK); 
-                    g.drawRect((int) p.getX(), (int) p.getY(), (int) celDem.getWidth() / split, (int) celDem.getHeight() / split);
+                } else if (temp == Color.BLACK) {
+                	g.fillRect((int) p.getX(), (int) p.getY(), (int) celDem.getWidth() / split, (int) celDem.getHeight() / split);
+                }
+                constructGrid(g, (int) p.getX(), (int) p.getY(), (int) celDem.getWidth() / split, (int) celDem.getHeight() / split);
             }
         }
+    }
+    
+    // Constructs a grid based on whether the user prefered it or not
+    public void constructGrid (Graphics g, int x, int y, int width, int height)
+    {
+    	if(gridOp) 
+    	{
+    		g.setColor(Color.BLACK);
+    		g.drawRect(x, y, width, height);
+    	}
+    	
     }
     
 	/*
@@ -112,9 +121,9 @@ public class Board extends JComponent implements MouseListener, ComponentListene
     /*
 	 * Determines the color of what a cell should be in Black and White values, based on the current state.
 	 */
-    public Color detCelColBW (int state) 
+    public Color detCelColBW (int colorSch) 
     {
-        if(state == 1) 
+        if(colorSch == 1) 
         {
             return Color.BLACK; 
         } else {
@@ -126,13 +135,13 @@ public class Board extends JComponent implements MouseListener, ComponentListene
 	 * Sets the board's colorSch that is used to determine if RGB or Black and White values will
 	 * be used.
 	 */
-    public void setColorSch(int choose)
+    public void setColorSch(int scolorSch)
     {
-    	if(choose == 1)
+    	if(colorSch == 1)
     	{
-    		colorSch = 1;
+    		this.colorSch = 1;
     	} else {
-    		colorSch = 2;
+    		this.colorSch = 2;
     	}
     }
     
@@ -142,24 +151,18 @@ public class Board extends JComponent implements MouseListener, ComponentListene
 	 */
     public void progressGen () 
     {
-    	for(int width = 0; width < split; width++)
-        {
-            for(int height = 0; height < split; height++)
-            {
-            	if(cel[width][height].getState() == 1)
-            	{
-            		tempCel[width][height] = new Cell(1);
-            	} else {
-            		tempCel[width][height] = new Cell();
-            	}
-            }
-        }
         for(int width = 0; width < split; width++)
         {
             for(int height = 0; height < split; height++)
             {
             	
                 calcNeighbors(width, height);
+            }
+        }
+        for(int width = 0; width < split; width++)
+        {
+            for(int height = 0; height < split; height++)
+            {
                 cel[width][height].updState();
             }
         }
@@ -185,7 +188,8 @@ public class Board extends JComponent implements MouseListener, ComponentListene
 	 */
     public void setCell(int x, int y) 
     {
-        cel[x][y] = new Cell(1);
+        cel[x][y] = new Cell(1, res, dead1, dead2);
+        repaint();
     }
     
     /*
@@ -199,9 +203,9 @@ public class Board extends JComponent implements MouseListener, ComponentListene
         	for(int vert = -1; vert < 2; vert++) 
         	{
         		try {
-        			if(tempCel[width+hori][height+vert].getState() == 1 && ((hori != 0 && vert != 0) || (hori != 0 && vert == 0) || (hori == 0 && vert != 0))) 
+        			if(cel[width+hori][height+vert].getState() == 1 && !(hori == 0 && vert == 0)) 
         			{
-        				cel[width][height].incrNeighbors(); 
+        				cel[width][height].incrNeighbors();
         			}
         		} catch(Exception e) {
         			
@@ -210,56 +214,125 @@ public class Board extends JComponent implements MouseListener, ComponentListene
         }
         
     }
-
+    
+	// repeats generations, used only by the timer in the gameoflife to automatically continue
+	public void repGen ()
+    {
+        for(int i = 0; i < genPerDelay; i++)
+        {
+        	resetAllNeigh();
+            progressGen();
+        }
+    }
+    
+    // Mouse click listener used to turn cells on and off directly from clicking it
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		loc = arg0.getLocationOnScreen();
-		if(cel[(int) loc.getX() / split][(int) loc.getY() / split].getState() == 0)
+	public void mouseClicked(MouseEvent arg0) 
+	{
+		loc1 = arg0.getLocationOnScreen();
+		loc1.setLocation(loc1.getX(),loc1.getY()-76);
+		if(cel[(int) (loc1.getY() / (celDem.getHeight() / split))][(int) (loc1.getX() / (celDem.getWidth() / split))].getState() == 0)
 		{
-			cel[(int) (loc.getX() / split)][(int) (loc.getY() / split)] = new Cell(1);
-			System.out.println(loc.getX());
-			System.out.println(split);
-			System.out.println(celDem.getWidth());
+			cel[(int) (loc1.getY() / (celDem.getHeight() / split))][(int) (loc1.getX() / (celDem.getWidth() / split))] = new Cell(1, res, dead1, dead2);
 		} else {
-			cel[(int) (loc.getX() / split)][(int) (loc.getY() / split)] = new Cell(0);
+			cel[(int) (loc1.getY() / (celDem.getHeight() / split))][(int) (loc1.getX() / (celDem.getWidth() / split))] = new Cell(0, res, dead1, dead2);
 		}
 		repaint();
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mouseEntered(MouseEvent arg0) {}
+	@Override
+	public void mouseExited(MouseEvent arg0) {}
+	@Override
+	public void mousePressed(MouseEvent arg0) 
+	{
+		loc1 = arg0.getLocationOnScreen();
+		loc1.setLocation(loc1.getX(), loc1.getY() - 76);
 	}
 
 	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mouseReleased(MouseEvent arg0) 
+	{
+		try {
+			loc2 = arg0.getLocationOnScreen();
+			loc2.setLocation(loc2.getX(), loc2.getY() - 76);
+			highlightCells();
+		} catch(Exception e) {
+			
+		}
 	}
 	
-	 /*
-	  * Used to give the dimension of the page after each resize.
-	  */
-	public void componentResized (ComponentEvent e)
-    {
-    	celDem = e.getComponent().getBounds().getSize();
-    }
+	// Prepares the board to be switched into different dimensions
+	public void prepSplit (int split)
+	{
+		this.split = split;
+		resetBoard();
+	}
+	
+	public void highlightCells()
+	{
+		for(int i = (int) (loc1.getY() / (celDem.getHeight() / split)); i < (int) (loc2.getY() / (celDem.getHeight() / split)); i++)
+		{
+			for(int x = (int) (loc1.getX() / (celDem.getWidth() / split)); x < (int) (loc2.getX() / (celDem.getWidth() / split)); x++)
+			{
+				if(cel[i][x].getState() == 0)
+				{
+					cel[i][x] = new Cell(1, res, dead1, dead2);
+				} else {
+					cel[i][x] = new Cell(0, res, dead1, dead2);
+				}
+			}
+		}
+		repaint();
+	}
+	
+	/*
+	 * Various getter and setter methods used to put values from JTextFields onto them, and then onto all or some of
+	 * the various cells.
+	 */
+	public void setDead1 (int dead1)
+	{
+		this.dead1 = dead1;
+	}
+	
+	public void setDead2 (int dead2)
+	{
+		this.dead2 = dead2;
+	}
+	
+	public void setCelDem (Dimension celDem)
+	{
+		this.celDem = celDem;
+	}
+	
+	public void setRes (int res)
+	{
+		this.res = res;
+	}
 
-	public void componentHidden(ComponentEvent e) {}
-	public void componentMoved(ComponentEvent e) {}
-	public void componentShown(ComponentEvent e) {}
+	public long getDelay() {
+		return delay;
+	}
+
+	public void setDelay(long delay) {
+		this.delay = delay;
+	}
+	
+	public int getGenPerDelay()
+	{
+		return genPerDelay;
+	}
+	
+	public void setGenPerDelay(int genPerDelay)
+	{
+		this.genPerDelay = genPerDelay;
+	}
+	
+	public void setGridOp (boolean gridOp)
+	{
+		this.gridOp = gridOp;
+		repaint();
+	}
 
 }
